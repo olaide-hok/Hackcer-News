21a243456f7e2d089e442cdd22fcc5cde388064c
olaide-hok <olaidekareem43@yahoo.com>
2022-09-23 22:02:42 +0100

    Setup Prev and Next Buttons
    Create handlePage fn and pass down via App provider.
    
    Add page to dependency array to re-invoke the API call.
    
    Create HANDLE_PAGE dispatch case in reducer.js
    
    Setup Buttons.js to handle pagination and render page numbers on the UI.

--- src/context.js
+++ src/context.js
@@ -52,14 +52,19 @@ const AppProvider = ({ children }) => {
     dispatch({ type: HANDLE_SEARCH, payload: query})
   }
 
+  const handlePage = (value) => {
+    dispatch({type: HANDLE_PAGE, payload: value})
+  }
+
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [state.query])
+  }, [state.query, state.page])
 
   return <AppContext.Provider value={{
     ...state,
     removeStory,
-    handleSearch
+    handleSearch,
+    handlePage
   }}>{children}</AppContext.Provider>
 }
 // make sure use
